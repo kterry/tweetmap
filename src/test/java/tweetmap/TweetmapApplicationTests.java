d9c1853ac3d201cdcde0915a9e0bebbb22e5b060
kterry <karem.terry@unican.es>
2015-02-15 21:40:28 +0100

    - rename package demo

--- src/test/java/tweetmap/TweetmapApplicationTests.java
+++ src/test/java/tweetmap/TweetmapApplicationTests.java
@@ -1,10 +1,12 @@
-package demo;
+package tweetmap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import tweetmap.TweetmapApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TweetmapApplication.class)
