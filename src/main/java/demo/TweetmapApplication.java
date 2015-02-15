92a3bebd25822c29381ebf883f5c053623ab0887
kterry <karem.terry@unican.es>
2015-02-15 21:02:11 +0100

    - First commit

---
+++ src/main/java/demo/TweetmapApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetmapApplication.class, args);
+    }
+}
